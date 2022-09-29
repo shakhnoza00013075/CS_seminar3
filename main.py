134933a5f2477341dbb00e4c9d3ceff5d5dd478f
wiut00013075 <wiut0001305@gmail.com>
2022-09-29 15:13:54 +0500

    init: seminar 3

---
+++ main.py
@@ -0,0 +1,7 @@
+def any_base_to_decimal( number, base):
+    temp=int(number, base)
+    print(temp)
+
+u_number=input("Enter a digit to convert")
+u_base = int(input("Enter a base"))
+any_base_to_decimal(u_number, u_base)
